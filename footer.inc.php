f00a24f4dfb7750965edb5722bf95880d2b15b5d
ahmeddrr <101930111+ahmeddrr@users.noreply.github.com>
2023-08-15 23:06:43 +0500

    Add files via upload

---
+++ footer.inc.php
@@ -0,0 +1,85 @@
+<footer>
+            <div class="footer-area padding-70px-tb md-padding-40px-tb xs-padding-30px-tb">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-3 col-sm-6 sm-margin-30px-bottom">
+                            <img src="img/logos/logo.png" class="width-70 margin-30px-bottom xs-margin-20px-bottom" alt="" />
+                            <address class="width-70 no-margin-bottom">74 Guild Street 542B, Great North Town MT.</address>
+                        </div>
+
+                        <div class="col-lg-3 col-sm-6 sm-margin-30px-bottom">
+                            <h3 class="footer-title-style1">Reservation</h3>
+                            <ul class="list-style-1 no-margin">
+                                <li>
+                                    <span><strong>Email:</strong> addyour@emailhere</span>
+                                </li>
+                                <li>
+                                    <span><strong>Tel:</strong> (+44) 123 456 789</span>
+                                </li>
+                            </ul>
+                        </div>
+
+                        <div class="col-lg-3 col-sm-6 mobile-margin-30px-bottom">
+                            <h3 class="footer-title-style1">Useful Links</h3>
+                            <ul class="list-style-1 no-margin-bottom">
+                                <li><a href="#!">Location</a></li>
+                                <li><a href="#!">Terms &amp; Conditions</a></li>
+                                <li><a href="#!">About Us</a></li>
+                            </ul>
+                        </div>
+
+                        <div class="col-lg-3 col-sm-6">
+                            <h3 class="footer-title-style1">Social Links</h3>
+                            <ul class="social-icon-style no-margin">
+                                <li>
+                                    <a href="javascript:void(0)"><i class="fab fa-facebook-f"></i></a>
+                                </li>
+                                <li>
+                                    <a href="javascript:void(0)"><i class="fab fa-twitter"></i></a>
+                                </li>
+                                <li>
+                                    <a href="javascript:void(0)"><i class="fab fa-instagram"></i></a>
+                                </li>
+                                <li>
+                                    <a href="javascript:void(0)"><i class="fab fa-linkedin-in"></i></a>
+                                </li>
+                            </ul>
+                        </div>
+
+                    </div>
+
+                </div>
+            </div>
+
+            <div class="footer-bar xs-padding-15px-tb">
+                <div class="container">
+                    <div class="text-center">
+                        <p>&copy; 2020 Fivestar is Powered by Chitrakoot Web</p>
+                    </div>
+                </div>
+            </div>
+        </footer>
+        <!-- end footer section -->
+
+    </div>
+    <!-- end main-wrapper section -->
+
+    <!-- start scroll to top -->
+    <a href="javascript:void(0)" class="scroll-to-top"><i class="fas fa-angle-up" aria-hidden="true"></i></a>
+    <!-- end scroll to top -->
+
+    <!-- all js include start -->
+
+    <!-- Java script -->
+    <script src="js/core.min.js"></script>
+	
+	<!-- Serch -->
+    <script src="search/search.js"></script>
+	
+    <!-- custom scripts -->
+    <script src="js/main.js"></script>
+
+    <!-- all js include end -->
+
+</body>
+</html>
---
+++ header.inc.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- metas -->
+    <meta charset="utf-8">
+    <meta name="author" content="Chitrakoot Web" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <meta name="keywords" content="Septosha Hotel Booking Bootstrap Template" />
+    <meta name="description" content="Fivestar - Hotel Booking Bootstrap Template" />
+    <!-- title  -->
+    <title>Project</title>
+    <!-- favicon -->
+    <link rel="shortcut icon" href="img/logos/favicon.png" />
+    <link rel="apple-touch-icon" href="img/logos/apple-touch-icon-57x57.png" />
+    <link rel="apple-touch-icon" sizes="72x72" href="img/logos/apple-touch-icon-72x72.png" />
+    <link rel="apple-touch-icon" sizes="114x114" href="img/logos/apple-touch-icon-114x114.png" />
+    <!-- plugins -->
+    <link rel="stylesheet" href="css/plugins.css" />
+    <!-- search css -->
+    <link rel="stylesheet" href="search/search.css" />
+    <!-- core style css -->
+    <link href="css/styles.css" rel="stylesheet" />
+</head>
+
+<body>
+
+    <!-- start page loading -->
+    <div id="preloader">
+        <div class="row loader">
+            <div class="loader-icon"></div>
+        </div>
+    </div>
+    <!-- end page loading -->
+
+    <!-- start main-wrapper section -->
+    <div class="main-wrapper">
+
+        <!-- start header section -->
+        <header>
+
+            <div class="navbar-default">
+
+                <!-- start top search -->
+                <div class="top-search bg-black">
+                    <div class="container">
+                  
+                    </div>
+                </div>
+                <!-- end top search -->
+                <div class="container">
+                    <div class="row align-items-center">
+                        <div class="col-12 col-lg-12">
+                            <div class="menu_area alt-font sm-padding-10px-tb">
+                                <nav class="navbar navbar-expand-lg navbar-light no-padding">
+
+                                    <div class="navbar-header navbar-header-custom">
+                                        <!-- start logo -->
+                                        <a href="index.php" class="navbar-brand logo4">logo</a>
+                                        <!-- end logo -->
+                                    </div>
+
+                                    <div class="navbar-toggler"></div>
+
+                                    <!-- start menu area -->
+                                    <ul class="navbar-nav ml-auto" id="nav" style="display: none;">
+                                        <li><a href="#">Home</a>            
+                                        </li>
+                                        <li><a href="about.php">About Us</a></li>
+                                        <!-- <li><a href="#">Pages</a>
+                                            <ul>
+                                                <li><a href="testimonial.php">Testimonial</a></li>
+                                                <li><a href="offer-packages.php">Offre Packages</a></li>
+                                                <li><a href="my-booking.php">My Booking</a></li>
+                                                <li><a href="restaurant.php">Restaurant</a></li>
+                                                <li><a href="#">Other</a>
+                                                    <ul>
+                                                        <li><a href="login.php">Login</a></li>
+                                                        <li><a href="registration.php">Registration</a></li>
+                                                        <li><a href="coming-soon.php">Coming Soon</a></li>
+                                                        <li><a href="404-page.php">404 Page</a></li>
+                                                    </ul>
+                                                </li>
+                                            </ul>
+                                        </li> -->
+                                        <li><a href="#">Venue</a>
+                                            <ul>
+                                                <li><a href="our-rooms.php">Ballroom</a></li>
+                                                <li><a href="single-room.php">Banquet</a></li>
+                                                <li><a href="single-room.php">Lawn</a></li>
+                                            </ul>
+                                        </li>
+                                        <!-- <li><a href="#">Gallery</a>
+                                            <ul>
+                                                <li><a href="gallery-style1.php">Gallery style1</a></li>
+                                                <li><a href="gallery-style2.php">Gallery style2</a></li>
+                                                <li><a href="gallery-masonry.php">Gallery Masonry</a></li>
+                                                <li><a href="single-gallery.php">Single Gallery</a></li>
+                                            </ul>
+                                        </li> -->
+                                        <!-- <li><a href="#">Blog</a>
+                                            <ul>
+                                                <li><a href="blog-grid.php">Blog Grid</a></li>
+                                                <li><a href="blog-details.php">Blog Detail</a></li>
+                                                <li><a href="blog-masonry.php">Blog Masonry</a></li>
+                                            </ul>
+                                        </li> -->
+                                        <li><a href="contact-us.php">Contact</a></li>
+                                    </ul>
+                                    <div class="account ml-auto">
+                                        <li>
+
+                                            <p>account</p>
+                                        </li>
+                                    </div>
+                                    <!-- end menu area -->
+
+                                    <!-- start atribute navigation -->
+                                    <!-- <div class="attr-nav sm-no-margin sm-margin-65px-right xs-margin-50px-right">
+                                        <ul class="no-margin-top">
+                                            <li class="search"><a href="#"><i class="fas fa-search"></i></a></li>
+                                        </ul>
+                                    </div> -->
+                                    <!-- end atribute navigation -->
+
+                                </nav>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+        </header>
